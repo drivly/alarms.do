3925e47da6520a97eb64b2cb1bc31f3d034e1904
sam-lippert <lippertz@gmail.com>
2023-06-26 20:39:22 -0500

    fromnow

--- worker.js
+++ worker.js
@@ -45,7 +45,7 @@ export class Alarm {
         unit.startsWith('w') ? 604800000 :
         unit.startsWith('y') ? 31449600000 :
         1
-      await this.state.storage.put('due', this.due = value * multiplier)
+      await this.state.storage.put('due', this.due = Date.now() + value * multiplier)
     }
     if (searchParams.has('every')) {
       await this.state.storage.put('every', this.every = parseInt(searchParams.get('every')))
